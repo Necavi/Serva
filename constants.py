4c1f7f465c1da0bbca4559922665ec69f527716b
necavi <necavi@0xf.org>
2015-05-09 15:18:50 -0700

    Removing legacy code in preparation for new version

--- constants.py
+++
@@ -1,8 +0,0 @@
-nick = "serva2"
-channels = ["#serva"]
-ircinfo = ("irc.esper.net",6667)
-mysqlhost = "localhost"
-## Choices are: chanmsg, privmsg, or both
-commandtype = "both"
-kinky = True
-commandtag = "!"
--- plugins/joinchannels.py
+++
@@ -1,5 +0,0 @@
-import constants
-import plugintemplate
-class joinchannels(plugintemplate.plugin):
-    def OnEnable(self):
-        self.main.b.Join(", ".join(constants.channels))
